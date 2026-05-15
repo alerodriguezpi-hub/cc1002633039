42c19c9ee64be744e877e59373bcebb99b5e5c35
alerodriguezpi-hub <alerodriguezpi@unal.edu.co>
2026-05-15 14:49:09 -0500

    CrudParte2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
@@ -13,37 +14,25 @@ class UserController extends Controller
      */
     public function index()
     {
-        $user = User::all();
+        $users=User::all();
         return view('users.index',compact('users'));
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request  $request 
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
         $user = new User();
-
-        $user-> name;
-        $user-> email;
-        $user-> password;
-        $user-> birthday;
-
-        $user = User::save();
-        return  redirect()->route('user.index');
+        $user->name=$request->name;   
+        $user->email=$request->email;   
+        $user->password=$request->password;   
+        $user->birthday=$request->birthday;   
+        $user->save();
+        return redirect()->route('users.index');
     }
 
     /**
@@ -52,21 +41,10 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
-        $user = User::findOrFail($user);
-        return view('')
+        $user = User::findOrFail($id);
+        return view('users.edit',compact('user'));
     }
 
     /**
@@ -78,25 +56,19 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $user = User::finOrFail($id);
-
-        $user-> name;
-        $user-> email;
-        $user-> password;
-        $user-> birthday;
-
-        $user = User::save();
-        return  redirect()->route('user.index');
+        $user = new User();
+        $user->name=$request->name;   
+        $user->email=$request->email;   
+        $user->password=$request->password;   
+        $user->birthday=$request->birthday;   
+        $user->save();
+        return redirect()->route('users.index');
     }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function destroy($id)
     {
-        //
+        $user = User::findOrFail($id);
+        $user->delete();
+        return redirect()->route('users.index');
     }
 }
